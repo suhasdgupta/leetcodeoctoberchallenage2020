5c6783bc462597c0c0f00ebf2b90b11e9f6a667f
Suhaas D <suhasdgupta55555@gmail.com>
2020-10-10 18:19:11 +0530

    Update MinimumNumberOfArrowsToBurstBalloons.java

--- MinimumNumberOfArrowsToBurstBalloons.java
+++ MinimumNumberOfArrowsToBurstBalloons.java
@@ -1,18 +1,17 @@
 class MinimumNumberOfArrowsToBurstBalloons {
     public int findMinArrowShots(int[][] points) {
-        if (points.length == 0) {
-            return 0;
-        }
-        Arrays.sort(points, (a, b) -> a[1] - b[1]);
-        int arrowPos = points[0][1];
-        int arrowCnt = 1;
-        for (int i = 1; i < points.length; i++) {
-            if (arrowPos >= points[i][0]) {
-                continue;
+        if(points.length == 0) return 0;
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+        int arrow = 1; // if more than 0 balloons then atleast 1 arrow to start
+        int end = points[0][1];
+        for(int i = 1; i < points.length; i++) {
+            if(points[i][0] > end) {
+                arrow++;
+                end = points[i][1];
+            } else {
+                end = Math.min(end, points[i][1]);
             }
-            arrowCnt++;
-            arrowPos = points[i][1];
         }
-        return arrowCnt;
+        return arrow;
     }
 }
